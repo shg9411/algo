6d58d8bd5f325a23e834d07c9089427249d34d24
shg9411 <shg9411@naver.com>
2020-02-24 03:52:43 +0900

    2020-02-24_algo

--- coding_py/tmp.py
+++ coding_py/tmp.py
@@ -1,5 +1,28 @@
-import sys
-
-
-a,b,c = sys.stdin.read(1),sys.stdin.read(1),sys.stdin.read(1)
-print(a,b,c)
+N=int(input())
+cnt=0
+for n in range(N):
+    word=input()
+    ls=[]
+    flag=False
+    while flag!=True:
+        n=len(word)
+        if n==1:
+            if word not in ls:
+                cnt+=1
+                flag=True
+                break
+            else:
+                flag=True
+                break
+        else:
+            for i in range(n):
+                if word[i] in ls:
+                    flag=True
+                    break
+                elif word[i+1]==word[i]:
+                    continue
+                elif word[i+1]!=word[i]:
+                    ls.append(word[i])
+                    word=word[i+1:]
+                    break
+print(cnt)
