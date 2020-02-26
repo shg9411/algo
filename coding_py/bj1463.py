04e0ae15102b7cb7f4bf5768090ddbc93f9923b1
shg9411 <shg9411@naver.com>
2020-02-26 19:09:37 +0900

    2020-02-26_algo

--- coding_py/bj1463.py
+++ coding_py/bj1463.py
@@ -1,24 +1,25 @@
 a = int(input())
 count = 0
-minimum=[a]
+temp = [a]
+
+
 def cal(a):
     list = []
     for i in a:
         list.append(i-1)
-        if i%3 == 0:
+        if i % 3 == 0:
             list.append(i/3)
-        if i%2 == 0:
+        if i % 2 == 0:
             list.append(i/2)
     return list
- 
+
+
 while True:
     if a == 1:
         print(count)
         break
-    temp = minimum[:]
-    minimum = []
-    minimum = cal(temp)
-    count +=1
-    if min(minimum) == 1:
+    temp = cal(temp)
+    count += 1
+    if min(temp) == 1:
         print(count)
         break
