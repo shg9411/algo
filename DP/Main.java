2471d2725b622094447e1a652dda709db557b966
shg9411 <shg9411@naver.com>
2020-03-04 15:15:07 +0900

    2020-03-04_algo

--- DP/Main.java
+++ DP/Main.java
@@ -1,56 +1,25 @@
 package DP;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		String[] array = new String[n];
-		for (int i = 0; i < array.length; i++) {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		String[] minusArr = str.split("\\-");
+		int result = Integer.parseInt(minusArr[0]);
 
-			array[i] = br.readLine();
-		}
-		Arrays.sort(array, new Comparator<String>() {
-			@Override
-			public int compare(String s1, String s2) {
-				if (s1.length() < s2.length()) {
-					return -1;
-				} else if (s1.length() > s2.length()) {
-					return 1;
-				} else {
-					String tmp1 = s1;
-					s1 = s1.replaceAll("[^0-9]", "");
-					int[] arr1 = new int[s1.length()];
-					int result1 = 0;
-					for (int i = 0; i < arr1.length; i++) {
-						result1 += (s1.charAt(i) - '0');
-					}
-					String tmp2 = s2;
-					s2 = s2.replaceAll("[^0-9]", "");
-					int[] arr2 = new int[s2.length()];
-					int result2 = 0;
-					for (int i = 0; i < arr2.length; i++) {
-						result2 += (s2.charAt(i) - '0');
-					}
-					if (result1 > result2) {
-						return 1;
-					} else if (result1 < result2) {
-						return -1;
-					} else {
-						return tmp1.compareTo(tmp2);
-					}
-				}
+		for (int i = 1; i < minusArr.length; i++) {
+			String[] plusArr = minusArr[i].split("\\+");
+			int sum = 0;
+
+			for (int j = 0; j < plusArr.length; j++) {
+				sum += Integer.parseInt(plusArr[j]);
 			}
-		});
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+			result -= sum;
 		}
+		System.out.println(result);
+		scanner.close();
 	}
-
 }
