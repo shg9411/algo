82da1da1906d2b707c7d6ab6712dcb7b6faa9fdc
shg9411 <shg9411@naver.com>
2020-03-04 14:43:54 +0900

    2020-03-04_algo

--- coding_py/cp/2.py
+++ coding_py/cp/2.py
@@ -1,27 +1,43 @@
+'''
+arr에 담긴 물건들에 대해서
+k개 이상을 선택하여
+t원 이하의 가격을 만들 수 있는 경우의 수를 출력
+'''
 answer = 0
-def solution(arr,k,t):
+
+
+def solution(arr, k, t):
+    # 가격순으로 정렬
     arr.sort()
     selected = [False for _ in range(len(arr))]
+    # 확인할 리스트
     res = []
+
     def select(idx):
         global answer
-        if sum(res)>t:
+        # 가격이 t원 초과면 답이 될 수 없기에 return
+        if sum(res) > t:
             return
-        if len(res)>=k:
-            answer+=1
-        for i in range(idx,len(arr)):
-            #if selected[i]:
-            #    continue
+        # 가격이 t원 이하이고 k개 이상 골랐기 때문에 정답이 될 수 있음
+        # return을 하지 않는 이유는 더 담아도 가격이 t원 이하일 수 있기 때문
+        if len(res) >= k:
+            answer += 1
+        for i in range(idx, len(arr)):
+            # 물건 선택
             selected[i] = True
             res.append(arr[i])
+            # 선택 후 재귀호출
             select(i+1)
+            # 물건 선택 취소
             selected[i] = False
+            # pop은 어차피 맨 뒤에 있는 원소를 제거하기 때문에 시간복잡도가 O(1)이라 ㄱㅊ
             res.pop()
     select(0)
     return answer
 
-arr1 = [1,1,1,1,1]
-k1 = 5	
+
+arr1 = [1, 3, 12, 0, 1]
+k1 = 3
 t1 = 10
 
-print(solution(arr1,k1,t1))
+print(solution(arr1, k1, t1))
--- coding_py/cp/3.py
+++ coding_py/cp/3.py
@@ -1,4 +1,14 @@
+'''
+각 집마다 제한시간과 그 시간 안에 가면 받을 수 있는 팁이 2차원 리스트에 존재,
+총 제한시간이 주어지고 한 칸 이동시마다 1초 소요
+최대로 팁을 받을 수 있는 경우에 팁을 출력
+'''
+
+# 문제를 풀 방법이 생각나지 않아서 가중치 작업 알고리즘 글을 보면서 시도했지만 시간안에 풀지 못함
+# 솔직히 너무 어려웠음
+
 def solution(r, delivery):
+    #선택 한번 했으면 다시 탐색하는 경우가 없게 하려고 함
     selected = [[False for _ in range(r)] for _ in range(r)]
     time = [[0 for _ in range(r)] for _ in range(r)]
     tip = [[0 for _ in range(r)] for _ in range(r)]
