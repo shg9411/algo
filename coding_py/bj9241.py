8968c525e07998c54227f92fbaa9f7e66d52c776
shg9411 <shg9411@naver.com>
2020-03-30 04:52:20 +0900

    2020-03-30_algo

--- coding_py/bj9241.py
+++ coding_py/bj9241.py
@@ -5,17 +5,17 @@ before = input().rstrip()
 after = input().rstrip()
 bl = len(before)
 al = len(after)
-length = min(al, bl)
+length = min(bl, al)
 idx = -1
 for i in range(length):
     idx = i
-    if after[i] != before[i]:
+    if before[i] != after[i]:
         break
 
 length -= idx
 diff = length
 for i in range(length):
-    if after[al-i-1] != before[bl-i-1]:
+    if before[bl-i-1] != after[al-i-1]:
         diff = i
         break
 
