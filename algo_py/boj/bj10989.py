4f4cad8bef1d32079eea7d1ac97f1b9bdf28ff5b
shg9411 <shg9411@naver.com>
2020-05-08 01:24:24 +0900

    2020-05-08_algo

--- algo_py/boj/bj10989.py
+++ algo_py/boj/bj10989.py
@@ -1,9 +1,21 @@
 import sys
-n = int(input())
-su = [0]*10001
-for _ in range(n):
-    su[int(sys.stdin.readline())] += 1
-
+n = 10000000
+sorted_nums = list()
+nums = [0]*10001
+_sum = [0]
+for i in range(n):
+    nums[i % 10000] += 1
 for i in range(1, 10001):
-    for _ in range(su[i]):
-        print(i)
+    _sum.append(_sum[i-1]+nums[i])
+    if _sum[i-1]+nums[i] == n:
+        break
+for i in range(len(_sum)-1, 0, -1):
+    for j in range(_sum[i]-_sum[i-1]):
+        sorted_nums.append(i)
+        n -= 1
+sorted_nums.reverse()
+for i in range(len(sorted_nums)):
+    # print(sorted_nums[i])
+    pass
+print(len(sorted_nums))
+print(sys.getsizeof(sorted_nums))
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,7 +1,20 @@
-import math
-a,b,c,d = map(int,input().split())
- 
-if math.ceil(c/b) <= math.ceil(a/d):
-  print("YES")
-else:
-  print("NO")
+import sys
+input = sys.stdin.readline
+MAX_NUM = 10000
+N = int(input())
+A = [int(input()) for _ in range(N)]
+count = [0]*(MAX_NUM+1)
+#print(sys.getsizeof(count))
+countSum = [0]*(MAX_NUM+1)
+for i in range(0, N):
+    count[A[i]] += 1
+countSum[0] = count[0]
+for i in range(1, MAX_NUM+1):
+    countSum[i] = countSum[i-1]+count[i]
+B = [0]*(N+1)
+for i in range(N-1, -1, -1):
+    B[countSum[A[i]]] = A[i]
+    countSum[A[i]] -= 1
+
+for i in range(1, N+1):
+    print(B[i])
