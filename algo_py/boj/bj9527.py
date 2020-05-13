19b854a2df8aafcc533ebeaee4d8537dcbd5282c
shg9411 <shg9411@naver.com>
2020-05-13 23:34:15 +0900

    2020-05-13_algo

---
+++ algo_py/boj/bj9527.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+
+def cnt(num):
+    #num += 1
+    tmp = num
+    t = 1
+    res = 0
+    while tmp:
+        if num & t:
+            res += num % t+(num-num % (t*2))//2
+        else:
+            res += (num-num % t)//2
+        t <<= 1
+        tmp >>= 1
+    return res
+
+
+print(cnt(b)-cnt(a-1))
