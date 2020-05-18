e6dfc391baef65e3a499998986df45ec17239837
shg9411 <shg9411@naver.com>
2020-05-19 08:07:38 +0900

    2020-05-19_algo

--- algo_py/boj/bj14502.py
+++ algo_py/boj/bj14502.py
@@ -1,75 +1,49 @@
 from collections import deque
-
-lab, list_virus = [], []
-N, M, NxM, maximum, origin_0_count, num_virus = 0, 0, 0, 0, 0, 0
-
-
-def spread(queue):
-    count = 0
-    while(queue):
-        virus = queue.popleft()
-        x, y = virus//M, virus % M
-        count += 1
-        if x+1 < N and lab[x+1][y] == 0:
-            lab[x+1][y] = 3
-            queue.append(virus+M)
-        if x-1 > -1 and lab[x-1][y] == 0:
-            lab[x-1][y] = 3
-            queue.append(virus-M)
-        if y+1 < M and lab[x][y+1] == 0:
-            lab[x][y+1] = 3
-            queue.append(virus+1)
-        if y-1 > -1 and lab[x][y-1] == 0:
-            lab[x][y-1] = 3
-            queue.append(virus-1)
-
-    for i in range(0, N):
-        for j in range(0, M):
-            if (lab[i][j] == 3):
-                lab[i][j] = 0
-    return count
-
-
-def check():
-    queue = deque(list_virus)
-    spreaded_count = spread(queue) - num_virus
-    return (origin_0_count-3)-spreaded_count
-
-
-def DFS(count, index):
-    if count == 3:
-        local = check()
-        global maximum
-        maximum = max(maximum, local)
-        return
-    if index == NxM:
-        return
-    for curr in range(index, NxM):
-        x, y = curr//M, curr % M
-        if lab[x][y] != 0:
-            continue
-        lab[x][y] = 1
-        DFS(count+1, curr+1)
-        lab[x][y] = 0
-
+from itertools import combinations
 
 if __name__ == '__main__':
-    N_M = [int(x) for x in input().split(' ')]
-    N, M = N_M[0], N_M[1]
-    NxM = N*M
-    count = 0
-    for i in range(0, N):
-        temp = [int(x) for x in input().split(' ')]
-        lab.append(temp)
-
-        for j in range(0, M):
-            if temp[j] != 0:
-                count += 1
-                if temp[j] == 2:
-                    viurs = i*M + j
-                    list_virus.append(viurs)
-
-    num_virus = len(list_virus)
-    origin_0_count = NxM - count
-    DFS(0, 0)
-    print(maximum)
+    N, M = map(int, input().split())
+    area = []
+    wall = []
+    virus = []
+    safe = N*M-3
+    res = 0
+    for i in range(N):
+        area.append(list(map(int, input().split())))
+        for j in range(M):
+            if area[i][j] == 0:
+                wall.append([i, j])
+                continue
+            safe -= 1
+            if area[i][j] == 2:
+                virus.append([i, j])
+    for comb in combinations(range(len(wall)), 3):
+        life = safe
+        for idx in comb:
+            x, y = wall[idx]
+            area[x][y] = 3
+        q = deque(virus)
+        while q:
+            x, y = q.popleft()
+            if x > 0 and area[x-1][y] == 0:
+                area[x-1][y] = 3
+                q.append([x-1, y])
+                life -= 1
+            if y > 0 and area[x][y-1] == 0:
+                area[x][y-1] = 3
+                q.append([x, y-1])
+                life -= 1
+            if x < N-1 and area[x+1][y] == 0:
+                area[x+1][y] = 3
+                q.append([x+1, y])
+                life -= 1
+            if y < M-1 and area[x][y+1] == 0:
+                area[x][y+1] = 3
+                q.append([x, y+1])
+                life -= 1
+        res = max(res, life)
+        for i in range(N):
+            for j in range(M):
+                if area[i][j] == 3:
+                    area[i][j] = 0
+    print(res)
---
+++ algo_py/boj/bj17141.py
@@ -0,0 +1,56 @@
+import sys
+import copy
+from collections import deque
+from itertools import combinations
+
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    life = N*N-M
+    area = []
+    virus = []
+    for i in range(N):
+        area.append(list(map(int, input().split())))
+        for j in range(N):
+            if area[i][j] == 2:
+                area[i][j] = 0
+                virus.append([i, j])
+            elif area[i][j] == 1:
+                life -= 1
+
+    res = 987654321
+    for comb in combinations(range(len(virus)), M):
+        tmpArea = copy.deepcopy(area)
+        tmpLife = life
+        q = deque()
+        for idx in comb:
+            x, y = virus[idx]
+            tmpArea[x][y] = 2
+            q.append(virus[idx])
+        tmpCnt = 0
+        while 1:
+            for _ in range(len(q)):
+                x, y = q.popleft()
+                if x > 0 and not tmpArea[x-1][y]:
+                    tmpArea[x-1][y] = 2
+                    tmpLife -= 1
+                    q.append([x-1, y])
+                if y > 0 and not tmpArea[x][y-1]:
+                    tmpArea[x][y-1] = 2
+                    tmpLife -= 1
+                    q.append([x, y-1])
+                if x < N-1 and not tmpArea[x+1][y]:
+                    tmpArea[x+1][y] = 2
+                    tmpLife -= 1
+                    q.append([x+1, y])
+                if y < N-1 and not tmpArea[x][y+1]:
+                    tmpArea[x][y+1] = 2
+                    tmpLife -= 1
+                    q.append([x, y+1])
+            if not q:
+                break
+            tmpCnt += 1
+        if tmpLife == 0:
+            res = min(res, tmpCnt)
+    print(res if res < 987654321 else -1)
