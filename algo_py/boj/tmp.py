51107eab2cec4205aedb08fb17552985e48ad9c2
shg9411 <shg9411@naver.com>
2020-05-20 23:22:40 +0900

    2020-05-20_algo

--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,24 +1,4 @@
-def minKBitFlips(A, K):
-    N = len(A)
-    hint = [0] * N
-    ans = flip = 0
+from collections import deque
 
-    # When we flip a subarray like A[i], A[i+1], ..., A[i+K-1]
-    # we can instead flip our current writing state, and put a hint at
-    # position i+K to flip back our writing state.
-    for i, x in enumerate(A):
-        flip ^= hint[i]
-        if x ^ flip == 0:  # If we must flip the subarray starting here...
-            ans += 1  # We're flipping the subarray from A[i] to A[i+K-1]
-            if i+K > N:
-                return -1  # If we can't flip the entire subarray, its impossible
-            flip ^= 1
-            if i+K < N:
-                hint[i + K] ^= 1
-
-    return ans
-
-
-a = [0, 0, 0, 0]
-k = 3
-print(minKBitFlips(a, k))
+A, B, K = map(int, input().split())
+q = deque()
