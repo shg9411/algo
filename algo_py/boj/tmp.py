6442e4926ae151cdcb252d38827b4e5945ef55f5
shg9411 <shg9411@naver.com>
2020-06-08 23:19:50 +0900

    2020-06-08_algo

--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,16 +1,6 @@
-import sys
-input=sys.stdin.readline
-
-T=int(input())
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    chk=[list(map(int, input().split()))]
-    cnt=K
-    for _ in range(K-1):
-        X,Y=map(int,input().split())
-        for [r,c] in chk:
-            if (r,c)==(X-1,Y) or (r,c)==(X+1,Y) or (r,c)==(X,Y-1) or (r,c)==(X,Y+1):
-                cnt-=1
-                break
-        chk.append([X,Y])
-    print(cnt)
+result = 0
+for i in range(int(input())):
+    word = input()
+    for char in word:
+        print(word.find(char),end=' ')
+print(result)
