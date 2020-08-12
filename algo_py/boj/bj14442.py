3751f2cd73dbe04a8add4caee7640653f61fb98f
shg9411 <shg9411@naver.com>
2020-08-12 20:00:48 +0900

    2020-08-12_algo

--- algo_py/boj/bj14442.py
+++ algo_py/boj/bj14442.py
@@ -4,9 +4,9 @@ from collections import deque
 input = sys.stdin.readline
 n, m, k = map(int, input().split())
 load = [input().rstrip() for _ in range(n)]
-visited = [[k+1 for _ in range(m)] for _ in range(n)]
+visited = [[[k+1 for _ in range(2)] for _ in range(m)] for _ in range(n)]
 nm = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-visited[0][0] = 0
+visited[0][0][0] = 0
 q = deque([[0, 0, 0, 0]])
 res = 1
 while q:
@@ -20,15 +20,11 @@ while q:
             nx = x+_x
             ny = y+_y
             if 0 <= nx < n and 0 <= ny < m:
-                if load[nx][ny] == '0' and visited[nx][ny] > c:
+                if load[nx][ny] == '1' and visited[nx][ny] > c+1:
+                    visited[nx][ny] = c+1
+                    q.append([nx, ny, c+1])
+                elif load[nx][ny] == '0' and visited[nx][ny] > c:
                     visited[nx][ny] = c
-                    q.append([nx, ny, 1 ^ s, c])
-                elif load[nx][ny] == '1':
-                    if visited[nx][ny] > c+1:
-                        if s == 0:
-                            visited[nx][ny] = c+1
-                            q.append([nx, ny, 1 ^ s, c+1])
-                        else:
-                            q.append([x, y, 1 ^ s, c])
+                    q.append([nx, ny, c])
     res += 1
 print(-1)
