e0a7a8e1721414e162e4f0d0466d807224c4c2e8
shg9411 <shg9411@naver.com>
2020-09-03 04:34:39 +0900

    2020-09-03_algo

--- algo_py/boj/bj10217.py
+++ algo_py/boj/bj10217.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 
 
@@ -10,22 +10,30 @@ def solve():
         u, v, c, d = map(int, input().split())
         adj[u].append((v, c, d))
     dist = [[sys.maxsize for _ in range(M+1)] for _ in range(N+1)]
+    q = []
     dist[1][0] = 0
-    for j in range(M+1):
-        for i in range(1, N+1):
-            if dist[i][j] == sys.maxsize:
+    heapq.heappush(q, (0, 0, 1))
+    res = -1
+    while q:
+        time, cost, here = heapq.heappop(q)
+        if dist[here][cost] < time:
+            continue
+        if here == N:
+            res = time
+            break
+        for nxt, nc, nt in adj[here]:
+            nt += time
+            nc += cost
+            if nc > M or dist[nxt][nc] <= nt:
                 continue
-            time = dist[i][j]
-            for nv, nc, nd in adj[i]:
-                if nc+j > M:
-                    continue
-                if dist[nv][nc+j] > time+nd:
-                    dist[nv][nc+j] = time+nd
-    res = min(dist[N])
-    if res == sys.maxsize:
-        print("Poor KCM")
-    else:
-        print(res)
+            for i in range(nc, M+1):
+                if dist[nxt][i] > nt:
+                    dist[nxt][i] = nt
+                else:
+                    break
+            heapq.heappush(q, (nt, nc, nxt))
+    print("Poor KCM" if res == -1 else res)
+    return
 
 
 if __name__ == '__main__':
