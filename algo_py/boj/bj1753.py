36d811b2204c074e7964858245fa8069eeea4369
shg9411 <shg9411@naver.com>
2020-09-04 21:38:19 +0900

    2020-09-04_algo

--- algo_py/boj/bj1753.py
+++ algo_py/boj/bj1753.py
@@ -3,30 +3,37 @@ import heapq
 input =sys.stdin.readline
 INF = sys.maxsize
 
-V, E = map(int, sys.stdin.readline().split())
-K = int(sys.stdin.readline())
-
-adj = [[] for _ in range(V + 1)]
-for _ in range(E):
-    u, v, w = map(int, input().split())
-    adj[u].append((v, w))
-
-dist = [INF for _ in range(V + 1)]
-dist[K] = 0
-q = [(0, K)]
-
-while q:
-    c, p = heapq.heappop(q)
-    if c > dist[p]:
-        continue
-    for v, w in adj[p]:
-        if dist[v] <= c+w:
+def solve():
+    V, E = map(int, sys.stdin.readline().split())
+    K = int(sys.stdin.readline())
+    
+    adj = [dict() for _ in range(V + 1)]
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        if v in adj[u]:
+            adj[u][v] = min(adj[u][v],w)
+        else:
+            adj[u][v] = w
+    
+    dist = [INF for _ in range(V + 1)]
+    dist[K] = 0
+    q = [(0, K)]
+    
+    while q:
+        c, p = heapq.heappop(q)
+        if c > dist[p]:
             continue
-        dist[v] = c+w
-        heapq.heappush(q, (c+w, v))
-
-for r in dist[1:]:
-    if r < sys.maxsize:
-        print(r)
-    else:
-        print("INF")
+        for v, w in adj[p].items():
+            if dist[v] <= c+w:
+                continue
+            dist[v] = c+w
+            heapq.heappush(q, (c+w, v))
+    
+    for r in dist[1:]:
+        if r < sys.maxsize:
+            print(r)
+        else:
+            print("INF")
+        
+if __name__=='__main__':
+    solve()
