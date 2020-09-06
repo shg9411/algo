8ad5696ecd05925297ba0d31c8a6168ee1e07005
shg9411 <shg9411@naver.com>
2020-09-06 20:12:55 +0900

    2020-09-06_algo

--- algo_py/boj/bj11724_dfs.py
+++ algo_py/boj/bj11724_dfs.py
@@ -2,28 +2,33 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
 
-edge = [[] for _ in range(n+1)]
-visited = set()
-count = 0
+def solve():
+    n, m = map(int, input().split())
+    edge = [set() for _ in range(n+1)]
+    visited = set()
+    count = 0
 
-
-def dfs(idx):
-    visited.add(idx)
-    for num in edge[idx]:
-        if num not in visited:
+    def dfs(idx):
+        visited.add(idx)
+        if len(visited) == n:
+            print(count)
+            exit()
+        for num in edge[idx]:
+            if num in visited:
+                continue
             dfs(num)
 
+    for _ in range(m):
+        u, v = map(int, input().split())
+        edge[u].add(v)
+        edge[v].add(u)
 
-for _ in range(m):
-    u, v = map(int, input().split())
-    edge[u].append(v)
-    edge[v].append(u)
+    for i in range(1, n+1):
+        if i not in visited:
+            count += 1
+            dfs(i)
 
-for i in range(1, n+1):
-    if i not in visited:
-        count += 1
-        dfs(i)
 
-print(count)
+if __name__ == '__main__':
+    solve()
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,15 +1,35 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-    n, x, y = map(int, input().split())
-    s = set()
-    s.add(x)
-    s.add(y)
-    n -= 2
-    if n == 0:
-        print(*s)
-        continue
-    i = abs(x-y)//n
-    print(i)
+
+def solve():
+    n, m = map(int, input().split())
+
+    edge = [[] for _ in range(n+1)]
+    visited = set()
+    count = 0
+
+    def dfs(idx):
+        visited.add(idx)
+        if len(visited) == n:
+            print(count)
+            exit()
+        for num in edge[idx]:
+            if num in visited:
+                continue
+            dfs(num)
+
+    for _ in range(m):
+        u, v = map(int, input().split())
+        edge[u].append(v)
+        edge[v].append(u)
+
+    for i in range(1, n+1):
+        if i not in visited:
+            count += 1
+            dfs(i)
+
+
+if __name__ == '__main__':
+    solve()
