ca87543168c43fd305c939db7ba435c5e6883fa4
shg9411 <shg9411@naver.com>
2020-09-10 22:29:45 +0900

    2020-09-10_algo

--- algo_py/boj/bj7576.py
+++ algo_py/boj/bj7576.py
@@ -2,39 +2,46 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-m, n = map(int, input().split())
-tomato = []
-ripen = deque()
-for i in range(n):
-    tomato.append(list(input().split()))
-    for j in range(m):
-        if tomato[i][j] == '1':
-            ripen.append((i, j))
-day = -1
-while ripen:
-    length = len(ripen)
-    for _ in range(length):
-        x, y = ripen.popleft()
-        if x > 0:
-            if tomato[x-1][y] == '0':
-                tomato[x-1][y] = '1'
-                ripen.append((x-1, y))
-        if y > 0:
-            if tomato[x][y-1] == '0':
-                tomato[x][y-1] = '1'
-                ripen.append((x, y-1))
-        if x < n-1:
-            if tomato[x+1][y] == '0':
-                tomato[x+1][y] = '1'
-                ripen.append((x+1, y))
-        if y < m-1:
-            if tomato[x][y+1] == '0':
-                tomato[x][y+1] = '1'
-                ripen.append((x, y+1))
-    day += 1
 
-for line in tomato:
-    if '0' in line:
+def solve():
+    m, n = map(int, input().split())
+    tomato = []
+    haveto = 0
+    tmt = deque()
+    for i in range(n):
+        tomato.append(input().split())
+        for j in range(m):
+            if tomato[i][j] == '0':
+                haveto += 1
+            elif tomato[i][j] == '1':
+                tmt.append((i, j))
+    res = 0
+    while tmt and haveto:
+        l = len(tmt)
+        for _ in range(l):
+            x, y = tmt.popleft()
+            if x > 0 and tomato[x-1][y] == '0':
+                tomato[x-1][y] = 1
+                tmt.append((x-1, y))
+                haveto -= 1
+            if y > 0 and tomato[x][y-1] == '0':
+                tomato[x][y-1] = 1
+                tmt.append((x, y-1))
+                haveto -= 1
+            if x < n-1 and tomato[x+1][y] == '0':
+                tomato[x+1][y] = 1
+                tmt.append((x+1, y))
+                haveto -= 1
+            if y < m-1 and tomato[x][y+1] == '0':
+                tomato[x][y+1] = 1
+                tmt.append((x, y+1))
+                haveto -= 1
+        res += 1
+    if haveto:
         print(-1)
-        exit()
-print(day)
+    else:
+        print(res)
+
+
+if __name__ == '__main__':
+    solve()
