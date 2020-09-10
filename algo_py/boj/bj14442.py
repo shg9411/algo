795e14875f02cbc0b6576c456f2606713f65c0df
shg9411 <shg9411@naver.com>
2020-09-10 21:38:11 +0900

    2020-09-10_algo

--- algo_py/boj/bj14442.py
+++ algo_py/boj/bj14442.py
@@ -1,30 +1,36 @@
-#pypy3
 import sys
 from collections import deque
 input = sys.stdin.readline
+nm = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 n, m, k = map(int, input().split())
-load = [input().rstrip() for _ in range(n)]
+if n == m == 1:
+    print(1)
+    exit()
+road = [input().rstrip() for _ in range(n)]
 visited = [[k+1 for _ in range(m)] for _ in range(n)]
-nm = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 visited[0][0] = 0
-q = deque([[0, 0, 0]])
+q = deque([(0, 0, 0)])
 res = 1
 while q:
-    qlen = len(q)
-    for _ in range(qlen):
+    l = len(q)
+    for _ in range(l):
         x, y, c = q.popleft()
-        if x == n-1 and y == m-1:
-            print(res)
-            exit()
         for _x, _y in nm:
-            nx = x+_x
-            ny = y+_y
-            if 0 <= nx < n and 0 <= ny < m:
-                if load[nx][ny] == '1' and visited[nx][ny] > c+1:
-                    visited[nx][ny] = c+1
-                    q.append([nx, ny, c+1])
-                elif load[nx][ny] == '0' and visited[nx][ny] > c:
-                    visited[nx][ny] = c
-                    q.append([nx, ny, c])
+            tx = x+_x
+            ty = y+_y
+            if 0 <= tx < n and 0 <= ty < m:
+                if x == n-1 and y == m-1:
+                    print(res)
+                    exit()
+                if visited[tx][ty] > c:
+                    if road[tx][ty] == '0':
+                        visited[tx][ty] = c
+                        q.append((tx, ty, c))
+                    else:
+                        if c >= k:
+                            continue
+                        visited[tx][ty] = c
+                        q.append((tx, ty, c+1))
     res += 1
 print(-1)
