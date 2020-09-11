421a855d57a12da6984a613504bc85983ebbde35
shg9411 <shg9411@naver.com>
2020-09-11 21:37:42 +0900

    2020-09-11_algo

--- algo_py/boj/bj1987.py
+++ algo_py/boj/bj1987.py
@@ -1,27 +1,31 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
 
-def dfs(i, j, cnt):
-    global result
-    for k in range(4):
-        i_tem = i + dx[k]
-        j_tem = j + dy[k]
-        if -1 < i_tem < R and -1 < j_tem < C and not bank[ord(data[i_tem][j_tem])-65]:
-            bank[ord(data[i_tem][j_tem])-65] = True
-            result = max(result, cnt+1)
-            dfs(i_tem, j_tem, cnt+1)
-            bank[ord(data[i_tem][j_tem])-65] = False
+def solve():
+    r, c = map(int, input().split())
+    bd = [[0 for _ in range(c)] for _ in range(r)]
+    nm = [(-1, 0), (0, 1), (0, -1), (1, 0)]
+    for i in range(r):
+        for j,x in enumerate(input()):
+            bd[i][j] = 1 << (ord(x)-65)
+    tm = [[0 for _ in range(c)] for _ in range(r)]
+    res = 1
+    q = deque([(0, 0, bd[0][0], 1)])
+    while q:
+        x, y, msk, l = q.popleft()
+        if l > res:
+            res = l
+        for _x, _y in nm:
+            tx = x+_x
+            ty = y+_y
+            if 0 <= tx < r and 0 <= ty < c:
+                if (tmsk := msk | bd[tx][ty]) == msk:
+                    continue
+                if tm[tx][ty] != tmsk:
+                    q.append((tx, ty, tmsk, l+1))
+                    tm[tx][ty] = tmsk
+    print(res)
 
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-R, C = map(int, input().split())
-data = [input().rstrip() for _ in range(R)]
-result = 1
-bank = [False for _ in range(26)]
-bank[ord(data[0][0])-65] = True
-dfs(0, 0, 1)
-print(result)
+if __name__ == '__main__':
+    solve()
