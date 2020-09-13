40e2368513b1763c9ea3a9d3b2fbbf1b96a80a59
shg9411 <shg9411@naver.com>
2020-09-13 21:46:23 +0900

    2020-09-13_algo

--- algo_py/boj/bj2243.py
+++ algo_py/boj/bj2243.py
@@ -1,27 +1,27 @@
 import sys
 input = sys.stdin.readline
-print(2<<20, 2<<21,2<<20<<1)
-fenwik = [0 for _ in range((2 << 20) << 1)]
+
+tree = [0 for _ in range(2 << 21)]
 
 
 def update(node, value):
-    node += 2 << 20
+    node += (2 << 20)
     while node:
-        fenwik[node] += value
+        tree[node] += value
         node >>= 1
 
 
 def query(node):
     i = 1
     while i < (2 << 20):
-        fenwik[i] -= 1
-        if fenwik[i << 1] >= node:
+        tree[i] -= 1
+        if tree[i << 1] >= node:
             i = i << 1
         else:
-            node -= fenwik[i << 1]
+            node -= tree[i << 1]
             i = i << 1 | 1
-    fenwik[i] -= 1
-    return i-2 << 20
+    tree[i] -= 1
+    return i-(2 << 20)
 
 
 n = int(input())
