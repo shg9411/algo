1b0d963ebca1bce31a9fedeb19b2015717aa6137
shg9411 <shg9411@naver.com>
2020-09-14 21:47:48 +0900

    2020-09-14_algo

---
+++ algo_py/boj/bj19639.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+x, y, m = map(int, input().split())
+lose = []
+potion = []
+for _ in range(x):
+    lose.append(int(input()))
+for _ in range(y):
+    potion.append(int(input()))
+
+if m+sum(potion) <= sum(lose):
+    print(0)
+    exit()
+
+i = j = 0
+hp = m//2
+cur = m
+while i < x and j < y:
+    if cur > hp:
+        print(-i-1)
+        cur -= lose[i]
+        i += 1
+    else:
+        print(j+1)
+        cur += potion[j]
+        j += 1
+
+while i < x:
+    print(-i-1)
+    i += 1
+while j < y:
+    print(j+1)
+    j += 1
