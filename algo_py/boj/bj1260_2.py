ec45f36419803458bcc842baff3eec8b1e84a9d9
shg9411 <shg9411@naver.com>
2020-09-15 22:42:50 +0900

    2020-09-15_algo

--- algo_py/boj/bj1260_2.py
+++ algo_py/boj/bj1260_2.py
@@ -1,47 +1,48 @@
 import sys
 input = sys.stdin.readline
 
-n, m, v = map(int, input().split())
 
-edge = [[] for _ in range(n+1)]
+def solve():
+    n, m, v = map(int, input().split())
+    edge = [[] for _ in range(n+1)]
+    for _ in range(m):
+        u, uu = map(int, input().split())
+        edge[u].append(uu)
+        edge[uu].append(u)
 
-for _ in range(m):
-    n1, n2 = map(int, input().split())
-    edge[n1].append(n2)
-    edge[n2].append(n1)
+    for i in range(1, n+1):
+        edge[i].sort()
 
-for i in range(len(edge)):
-    edge[i].sort()
+    visit = [0 for _ in range(n+1)]
 
-visit = [0] * (n+1)
-
-
-def dfs(v):
-    visit[v] = 1
-    dfsRes.append(v)
-    for e in edge[v]:
-        if visit[e] == 0:
-            visit[e] = 1
-            dfs(e)
-
-
-def bfs(v):
-    visit = [0] * (n+1)
-    visit[v] = 1
-    bfsRes.append(v)
-    q = [v]
-    while q:
-        tmp = q.pop(0)
-        for e in edge[tmp]:
-            if visit[e] == 0:
-                bfsRes.append(e)
-                q.append(e)
+    def dfs(v):
+        visit[v] = 1
+        dfsRes.append(v)
+        for e in edge[v]:
+            if not visit[e]:
                 visit[e] = 1
+                dfs(e)
+
+    def bfs(v):
+        visit = [0 for _ in range(n+1)]
+        visit[v] = 1
+        bfsRes.append(v)
+        q = [v]
+        while q:
+            tmp = q.pop(0)
+            for e in edge[tmp]:
+                if not visit[e]:
+                    bfsRes.append(e)
+                    q.append(e)
+                    visit[e] = 1
+
+    dfsRes = []
+    bfsRes = []
+    dfs(v)
+    bfs(v)
+    print(*dfsRes)
+    print(*bfsRes)
 
 
-dfsRes = []
-bfsRes = []
-dfs(v)
-bfs(v)
-print(*dfsRes)
-print(*bfsRes)
+if __name__ == "__main__":
+    solve()
