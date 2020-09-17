896769ab11f0674fabcc0b2192ebf7c66c46fea8
shg9411 <shg9411@naver.com>
2020-09-17 19:10:24 +0900

    2020-09-17_algo

--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,31 +1,43 @@
+import sys
 from collections import deque
+from pprint import pprint as pp
+
+input: () = lambda: sys.stdin.readline().strip()
+#sys.stdin = open('inputs/boj/7576_input')
 
 
-def solve():
-    n, k = map(int, input().split())
-    if k <= n:
-        print(n-k)
+def solution(graph, queue):
+    dir = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    # 혹시 모를 visited 행렬 구현
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+
+    while queue:
+        r, c = queue.popleft()
+        for dr, dc in dir:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] == 0:
+                graph[nr][nc] = graph[r][c] + 1
+                visited[nr][nc] = visited[r][c] + 1
+                queue.append(tuple([nr, nc]))
+
+    if 0 in graph:
+        return -1
     else:
-        MAX = 100001
-        dist = [MAX for _ in range(MAX)]
-        dist[k] = 0
-        q = deque([k])
-        while q:
-            t = q.popleft()
-            if t == n:
-                break
-            x = t >> 1
-            if not t % 2 and dist[x] > dist[t]:
-                dist[x] = dist[t]
-                q.appendleft(x)
-            if t >= 1 and dist[t-1] > dist[t]+1:
-                dist[t-1] = dist[t]+1
-                q.append(t-1)
-            if t+1 < MAX and dist[t+1] > dist[t]+1:
-                dist[t+1] = dist[t]+1
-                q.append(t+1)
-        print(dist[n])
+        return max(max(visited))
 
 
-if __name__ == "__main__":
-    solve()
+if __name__ == '__main__':
+    m, n = map(int, input().split())
+    farm = []
+    queue = deque()
+    for i in range(n):
+        line = list(map(int, input().split()))
+        for j in range(m):
+            if line[j] == 1:
+                # 1인 경우에 큐에 넣어준다.
+                queue.append(tuple([i, j]))
+
+        farm.append(line)
+
+    result = solution(farm, queue)
+    print(result)
