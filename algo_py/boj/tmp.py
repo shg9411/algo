b89268c8ae0199801d71e97906c3ff9269f1707a
shg9411 <shg9411@naver.com>
2020-09-19 00:49:00 +0900

    2020-09-19_algo

--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,45 +1,8 @@
-import sys
-import heapq
-input = sys.stdin.readline
-
-
-def solve():
-    n = int(input())
-    m = int(input())
-    adj = [dict() for _ in range(n+1)]
-    for _ in range(m):
-        x, y, c = map(int, input().split())
-        if (v := adj[x].get(y)):
-            adj[x][y] = min(c, v)
-        else:
-            adj[x][y] = c
-    s, e = map(int, input().split())
-    dist = [sys.maxsize for _ in range(n+1)]
-    tmp = []
-    dist[s] = 0
-    index = [0 for _ in range(n+1)]
-    q = [(0, s)]
-    while q:
-        cost, post = heapq.heappop(q)
-        if cost > dist[post]:
-            continue
-        for p, c in adj[post].items():
-            c += cost
-            if c < dist[p]:
-                dist[p] = c
-                index[p] = post
-                heapq.heappush(q, (c, p))
-    res = []
-    tmp = e
-    while tmp:
-        res.append(tmp)
-        tmp = index[tmp]
-    money = dist[e]
-    res = res[::-1]
-    print(money)
-    print(len(res))
-    print(*res)
-
-
-if __name__ == '__main__':
-    solve()
+f, s, g, u, d = map(int, input().split())
+r = 0
+while r < f and 0 < s and s <= f:
+    if s == g:
+        print(r)
+        exit()
+    s -= d if s > g and s > d or s < g and s+u > f else -u
+print("use the stairs")
