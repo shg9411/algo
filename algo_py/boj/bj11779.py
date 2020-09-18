ce1f41757b3d073e01df6a21fe2fa969b2ebefcb
shg9411 <shg9411@naver.com>
2020-09-18 23:42:00 +0900

    2020-09-18_algo

--- algo_py/boj/bj11779.py
+++ algo_py/boj/bj11779.py
@@ -3,13 +3,22 @@ import heapq
 input = sys.stdin.readline
 
 
-def dijkstra(s, e):
+def solve():
+    n = int(input())
+    m = int(input())
+    adj = [dict() for _ in range(n+1)]
+    for _ in range(m):
+        x, y, c = map(int, input().split())
+        if (v := adj[x].get(y)):
+            adj[x][y] = min(c, v)
+        else:
+            adj[x][y] = c
+    s, e = map(int, input().split())
     dist = [sys.maxsize for _ in range(n+1)]
     tmp = []
     dist[s] = 0
     index = [0 for _ in range(n+1)]
-    q = []
-    heapq.heappush(q, [0, s])
+    q = [(0, s)]
     while q:
         cost, post = heapq.heappop(q)
         if cost > dist[post]:
@@ -19,27 +28,18 @@ def dijkstra(s, e):
             if c < dist[p]:
                 dist[p] = c
                 index[p] = post
-                heapq.heappush(q, [c, p])
+                heapq.heappush(q, (c, p))
     res = []
     tmp = e
     while tmp:
         res.append(tmp)
         tmp = index[tmp]
-    return dist[e], res[::-1]
+    money = dist[e]
+    res = res[::-1]
+    print(money)
+    print(len(res))
+    print(*res)
 
 
-n = int(input())
-m = int(input())
-adj = [dict() for _ in range(n+1)]
-for _ in range(m):
-    x, y, c = map(int, input().split())
-    if adj[x].get(y):
-        adj[x][y] = min(c, adj[x].get(y))
-    else:
-        adj[x][y] = c
-
-s, e = map(int, input().split())
-money, res = dijkstra(s, e)
-print(money)
-print(len(res))
-print(*res)
+if __name__ == '__main__':
+    solve()
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,20 +1,45 @@
 import sys
+import heapq
 input = sys.stdin.readline
-n = int(input())
-p = []
-for _ in range(n):
-    a, b = map(int, input().split())
-    if a > b:
-        a, b = b, a
-    p.append((a, b))
-s = p[0][0]
-e = p[0][1]
-c = e-s
-for x, y in p[1:]:
-    if e > x:
-        c += y-e if y > e else 0
-        e = max(e, y)
-    elif e <= x:
-        c += y-x
-        e = x
-print(c)
+
+
+def solve():
+    n = int(input())
+    m = int(input())
+    adj = [dict() for _ in range(n+1)]
+    for _ in range(m):
+        x, y, c = map(int, input().split())
+        if (v := adj[x].get(y)):
+            adj[x][y] = min(c, v)
+        else:
+            adj[x][y] = c
+    s, e = map(int, input().split())
+    dist = [sys.maxsize for _ in range(n+1)]
+    tmp = []
+    dist[s] = 0
+    index = [0 for _ in range(n+1)]
+    q = [(0, s)]
+    while q:
+        cost, post = heapq.heappop(q)
+        if cost > dist[post]:
+            continue
+        for p, c in adj[post].items():
+            c += cost
+            if c < dist[p]:
+                dist[p] = c
+                index[p] = post
+                heapq.heappush(q, (c, p))
+    res = []
+    tmp = e
+    while tmp:
+        res.append(tmp)
+        tmp = index[tmp]
+    money = dist[e]
+    res = res[::-1]
+    print(money)
+    print(len(res))
+    print(*res)
+
+
+if __name__ == '__main__':
+    solve()
