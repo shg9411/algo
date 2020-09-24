d23cb53710f294f444c1264ba215c1bc59c6387b
shg9411 <shg9411@naver.com>
2020-09-24 20:26:47 +0900

    2020-09-24_algo

--- algo_py/boj/json_test.py
+++ algo_py/boj/json_test.py
@@ -3,34 +3,42 @@ when using form
 headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 if needed
-headers["Authorization"] = "Bearer {token}"
+headers["Authorization"] = "{종류} {token}"
 '''
 
 import requests
+import json
 from requests.structures import CaseInsensitiveDict
 
 
 def GET(url, param=None):
-    # r.text, r.json(), etc..
     headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Content-Type"] = "application/json"
-    headers["Authorization"] = "123"
+    #headers["Accept"] = "application/json"
+    #headers["Content-Type"] = "application/json"
+    #headers["Authorization"] = ""
     r = requests.get(url, headers=headers, params=param)
     print(r.status_code)
     print(r.headers)
-    print(r.text)
+    try:
+        print(r.json())
+    except:
+        print(r.text)
+    # r.text, r.json(), r.content, r.encoding, etc..
 
 
 def POST(url, data=None):
     headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Content-Type"] = "application/json"
-    r = requests.post(url, headers=headers, data=data)
+    #headers["Accept"] = "application/vnd.github.baptiste-preview+json"
+    #headers["Content-Type"] = "application/json"
+    #headers["Authorization"] = ""
+    r = requests.post(url, headers=headers, data=json.dumps(data))
     print(r.status_code)
     print(r.headers)
-    print(r.text)
+    try:
+        print(r.json())
+    except:
+        print(r.text)
 
 
-POST("https://httpbin.org/response-headers?freeform=as")
-GET("https://httpbin.org/response-headers?freeform=d")
+# POST("https://api.github.com/repos/shg9411/json_test")
+GET("https://api.github.com/users/shg9411/repos")
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,53 +1,24 @@
-import sys
-sys.setrecursionlimit(10**9)
-input = sys.stdin.readline
-
-
-if __name__ == '__main__':
-    def dfs(u):
-        for v in adj[u]:
-            if not visited[v]:
-                visited[v] = True
-                dfs(v)
-        stack.append(u)
-
-    def get(u, v):
-        scc[v].append(u)
-        for n in adj_r[u]:
-            if not visited[n]:
-                visited[n] = True
-                get(n, v)
-
-    V, E = map(int, input().split())
-    adj = [[] for _ in range(V+1)]
-    adj_r = [[] for _ in range(V+1)]
-    scc = [[] for _ in range(V+1)]
-    stack = []
-
-    res = 0
-    for i in range(E):
-        u, v = map(int, input().split())
-        adj[u].append(v)
-        adj_r[v].append(u)
-    visited = [False for _ in range(V+1)]
-    for i in range(1, V+1):
-        if not visited[i]:
-            visited[i] = True
-            dfs(i)
-    visited = [False for _ in range(V+1)]
-
-    while stack:
-        tmp = stack.pop()
-        if not visited[tmp]:
-            visited[tmp] = True
-            get(tmp, res)
-            res += 1
-    res = []
-    for c in scc:
-        if c:
-            c.sort()
-            res.append(c)
-    res.sort()
-    print(len(res))
-    for c in res:
-        print(' '.join(map(str, c)), end=' -1\n')
+from collections import deque
+maxV = 10**6+1
+x = int(input())
+visited = [maxV for _ in range(x+1)]
+visited[x] = 0
+q = deque()
+q.append(x)
+while q:
+    tmp = q.popleft()
+    val = visited[tmp]
+    if tmp == 1:
+        print(val)
+        break
+    if tmp % 3 == 0:
+        if visited[tmp//3] > val+1:
+            visited[tmp//3] = val+1
+            q.append(tmp//3)
+    if tmp % 2 == 0:
+        if visited[tmp//2] > val+1:
+            visited[tmp//2] = val+1
+            q.append(tmp//2)
+    if visited[tmp-1] > val+1:
+        visited[tmp-1] = val+1
+        q.append(tmp-1)
