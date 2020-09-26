a3bc029960cd34425176093e2e5fcb18bec5e416
shg9411 <shg9411@naver.com>
2020-09-26 22:37:17 +0900

    2020-09-26_algo

--- algo_py/boj/bj1005.py
+++ algo_py/boj/bj1005.py
@@ -1,28 +1,26 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
 def solve():
     def dfs(u):
+        if dp[u] != -1:
+            return dp[u]
         m = 0
-        # 선행 작업 중 최대시간 추적
         for v in adj[u]:
-            m = max(m, t[v] if vi[v] else dfs(v))
-        vi[u] = 1
-        # 건설 시간 + 선행시간 중 최대 시간
-        t[u] += m
-        return t[u]
+            if dp[v] == -1:
+                dp[v] = dfs(v)
+            m = max(m, dp[v])
+        return m+t[u]
 
     n, k = map(int, input().split())
-    adj = [[] for _ in range(n+1)]
-    # 방문
-    vi = [0 for _ in range(n+1)]
-    # base 시간
+    adj = [set() for _ in range(n+1)]
+    dp = [-1 for _ in range(n+1)]
     t = [0]+list(map(int, input().split()))
     for _ in range(k):
         x, y = map(int, input().split())
-        adj[y].append(x)
+        adj[y].add(x)
     w = int(input())
     print(dfs(w))
 
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,48 +1,30 @@
 import sys
-import heapq
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-def s():
-    def g(s, e):
-        d = [sys.maxsize for _ in range(n+1)]
-        t = []
-        d[s] = 0
-        index = [0 for _ in range(n+1)]
-        q = []
-        heapq.heappush(q, [0, s])
-        while q:
-            cost, post = heapq.heappop(q)
-            if cost > d[post]:
-                continue
-            for p, c in adj[post].items():
-                c += cost
-                if c < d[p]:
-                    d[p] = c
-                    index[p] = post
-                    heapq.heappush(q, [c, p])
-        r = []
-        t = e
-        while t:
-            r.append(t)
-            t = index[t]
-        return d[e], r[::-1]
+def solve():
+    def dfs(u):
+        if dp[u] != -1:
+            return dp[u]
+        m = 0
+        for v in adj[u]:
+            if dp[v] == -1:
+                dp[v] = dfs(v)
+            m = max(m, dp[v])
+        return m+t[u]
 
-    n = int(input())
-    m = int(input())
-    adj = [dict() for _ in range(n+1)]
-    for _ in range(m):
-        x, y, c = map(int, input().split())
-        if adj[x].get(y):
-            adj[x][y] = min(c, adj[x].get(y))
-        else:
-            adj[x][y] = c
-
-    s, e = map(int, input().split())
-    m, r = g(s, e)
-    print(m, len(r), sep='\n')
-    print(*r)
+    n, k = map(int, input().split())
+    adj = [set() for _ in range(n+1)]
+    dp = [-1 for _ in range(n+1)]
+    t = [0]+list(map(int, input().split()))
+    for _ in range(k):
+        x, y = map(int, input().split())
+        adj[y].add(x)
+    w = int(input())
+    print(dfs(w))
 
 
-if __name__ == '__main__':
-    s()
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
