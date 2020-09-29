bf994464f25eb193d8ae73ce1138c8081a77747b
shg9411 <shg9411@naver.com>
2020-09-30 03:10:35 +0900

    2020-09-30_algo

--- algo_py/boj/bj5719.py
+++ algo_py/boj/bj5719.py
@@ -4,49 +4,57 @@ from collections import deque
 input = sys.stdin.readline
 
 
-def dijkstra(s, e):
-    dist = [sys.maxsize for _ in range(n)]
-    dist[s] = 0
-    q = [(0, s)]
-    tmp = [set() for _ in range(n)]
-    while q:
-        cost, des = heapq.heappop(q)
-        for to in adj[des]:
-            val = cost+adj[des][to]
-            if dist[to] < val:
+def solve():
+    def dijkstra(s, e):
+        dist = [sys.maxsize for _ in range(n)]
+        dist[s] = 0
+        q = [(0, s)]
+        while q:
+            cost, des = heapq.heappop(q)
+            if dist[des] != cost:
                 continue
-            dist[to] = val
-            tmp[to].add(des)
-            heapq.heappush(q, (val, to))
-    q = deque([e])
-    while q:
-        cur = q.popleft()
-        for pre in tmp[cur]:
-            if cur in adj[pre] and dist[cur] == adj[pre][cur] + dist[pre]:
-                q.append(pre)
-                adj[pre].pop(cur)
-    dist = [sys.maxsize for _ in range(n)]
-    dist[s] = 0
-    q = [(0, s)]
-    while q:
-        cost, des = heapq.heappop(q)
-        for to in adj[des]:
-            val = cost+adj[des][to]
-            if dist[to] <= val:
+            if des == e:
+                break
+            for to, val in adj[des].items():
+                val += cost
+                if dist[to] > val:
+                    dist[to] = val
+                    heapq.heappush(q, (val, to))
+        q = deque([e])
+        while q:
+            des = q.popleft()
+            for i in range(n):
+                if des in adj[i] and dist[i]+adj[i][des] == dist[des]:
+                    adj[i].pop(des)
+                    q.append(i)
+        dist = [sys.maxsize for _ in range(n)]
+        dist[s] = 0
+        q = [(0, s)]
+        while q:
+            cost, des = heapq.heappop(q)
+            if dist[des] != cost:
                 continue
-            dist[to] = val
-            heapq.heappush(q, (val, to))
-    return dist[e]
+            if des == e:
+                break
+            for to, val in adj[des].items():
+                val += cost
+                if dist[to] > val:
+                    dist[to] = val
+                    heapq.heappush(q, (val, to))
+        return dist[e]
+
+    while 1:
+        n, m = map(int, input().split())
+        if n == m == 0:
+            break
+        s, e = map(int, input().split())
+        adj = [dict() for _ in range(n)]
+        for _ in range(m):
+            u, v, p = map(int, input().split())
+            adj[u][v] = p
+        val = dijkstra(s, e)
+        print(val if val != sys.maxsize else -1)
 
 
-while 1:
-    n, m = map(int, input().split())
-    if n == m == 0:
-        break
-    s, d = map(int, input().split())
-    adj = [dict() for _ in range(n)]
-    for _ in range(m):
-        u, v, p = map(int, input().split())
-        adj[u][v] = p
-    val = dijkstra(s, d)
-    print(val if val != sys.maxsize else -1)
+if __name__ == '__main__':
+    solve()
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,11 +1,56 @@
 import sys
-c = 0
-for r in sys.stdin.read().splitlines()[1:-1]:
-    if r == '문제':
-        c += 1
-    else:
-        if c > 0:
-            c -= 1
-        else:
-            c += 2
-print("힝구" if c else "고무오리야 사랑해")
+import heapq
+from collections import deque
+input = sys.stdin.readline
+
+
+def dijkstra(s, e):
+    dist = [sys.maxsize for _ in range(n)]
+    dist[s] = 0
+    q = [(0, s)]
+    while q:
+        cost, des = heapq.heappop(q)
+        if dist[des] != cost:
+            continue
+        if des == e:
+            break
+        for to, val in adj[des].items():
+            val += cost
+            if dist[to] > val:
+                dist[to] = val
+                heapq.heappush(q, (val, to))
+    q = deque([e])
+    while q:
+        des = q.popleft()
+        for i in range(n):
+            if des in adj[i] and dist[i]+adj[i][des] == dist[des]:
+                adj[i].pop(des)
+                q.append(i)
+    dist = [sys.maxsize for _ in range(n)]
+    dist[s] = 0
+    q = [(0, s)]
+    while q:
+        cost, des = heapq.heappop(q)
+        if dist[des] != cost:
+            continue
+        if des == e:
+            break
+        for to, val in adj[des].items():
+            val += cost
+            if dist[to] > val:
+                dist[to] = val
+                heapq.heappush(q, (val, to))
+    return dist[e]
+
+
+while 1:
+    n, m = map(int, input().split())
+    if n == m == 0:
+        break
+    s, e = map(int, input().split())
+    adj = [dict() for _ in range(n)]
+    for _ in range(m):
+        u, v, p = map(int, input().split())
+        adj[u][v] = p
+    val = dijkstra(s, e)
+    print(val if val != sys.maxsize else -1)
