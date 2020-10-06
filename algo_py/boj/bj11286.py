e7c029d090154134f933e635165305a6f661d433
shg9411 <shg9411@naver.com>
2020-10-07 00:08:58 +0900

    2020-10-07_algo

--- algo_py/boj/bj11286.py
+++ algo_py/boj/bj11286.py
@@ -1,16 +1,17 @@
 import sys
 import heapq
 input = sys.stdin.readline
-n = int(input())
-q = []
-res = []
-for _ in range(n):
-    a = int(input())
-    if a != 0:
-        heapq.heappush(q, (abs(a), a))
-    else:
-        try:
-            res.append(heapq.heappop(q)[1])
-        except:
-            res.append(0)
-print('\n'.join(str(i) for i in res))
+def solve():
+    n = int(input())
+    q = []
+    res = []
+    for _ in range(n):
+        a = int(input())
+        if a != 0:
+            heapq.heappush(q, (abs(a), a))
+        else:
+            res.append(heapq.heappop(q)[1] if q else 0)
+    print('\n'.join(str(i) for i in res))
+
+if __name__=='__main__':
+    solve()
