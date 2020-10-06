70ba1c7d38f8891b1ab4e38b312434e41e69af34
shg9411 <shg9411@naver.com>
2020-10-06 18:44:28 +0900

    2020-10-06_algo

--- algo_py/boj/bj14889.py
+++ algo_py/boj/bj14889.py
@@ -1,43 +1,25 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+def solve(idx, cnt, diff):
+    global res
+    if cnt == n//2:
+        res = min(res, abs(diff))
+        return
+    if idx == n:
+        return
+    solve(idx+1, cnt+1, diff-r[idx]-c[idx])
+    solve(idx+1, cnt, diff)
+
 
 n = int(input())
-
-score = [list(map(int, input().split())) for _ in range(n)]
-
-selected = [False for _ in range(n)]
-
-minV = 1000000
-
-
-def calc():
-    global minV
-    ls = [i for i, v in enumerate(selected) if v == False]
-    ss = [i for i, v in enumerate(selected) if v == True]
-    tmps = 0
-    tmpl = 0
-    for i in range(n//2):
-        for j in range(i+1, n//2):
-            si = ss[i]
-            sj = ss[j]
-            li = ls[i]
-            lj = ls[j]
-            tmps += (score[si][sj]+score[sj][si])
-            tmpl += (score[li][lj]+score[lj][li])
-    minV = min(minV, abs(tmps-tmpl))
-
-
-def select(count, idx):
-    if count == n//2:
-        calc()
-        return
-    for i in range(idx, n):
-        # if not selected[i]:
-        selected[i] = True
-        select(count+1, i+1)
-        selected[i] = False
-
-
-select(0, 0)
-print(minV)
+s = [[0]*n for _ in range(n)]
+r = [0]*n
+c = [0]*n
+t = 0
+for i in range(n):
+    s[i] = list(map(int, input().split()))
+    for j in range(n):
+        t += s[i][j]
+        r[i] += s[i][j]
+        c[j] += s[i][j]
+res = 987654321
+solve(1, 1, t-r[0]-c[0])
+print(res)
