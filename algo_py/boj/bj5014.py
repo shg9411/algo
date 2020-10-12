9b4ce075d047157bc52e0ec7e3a2434588df8c8c
shg9411 <shg9411@naver.com>
2020-10-12 17:37:36 +0900

    2020-10-12_algo

--- algo_py/boj/bj5014.py
+++ algo_py/boj/bj5014.py
@@ -1,32 +1,9 @@
-import sys
-from collections import deque
-F, S, G, U, D = map(int, input().split())
-if S==G:
-    print(0)
-    exit()
-if S < G and U == 0 or S > G and D == 0:
-    print("use the stairs")
-    exit()
-gogo = [0 for _ in range(F+1)]
-visited = [False for _ in range(F+1)]
-gogo[S] = 0
-visited[S] = True
-deq = deque()
-deq.append(S)
-while deq:
-    tmp = deq.popleft()
-    count = gogo[tmp]
-    if tmp == G:
-        break
-    tmp1 = tmp+U
-    tmp2 = tmp-D
-    if tmp1 < F+1 and not visited[tmp1]:
-        visited[tmp1] = True
-        gogo[tmp1] = count+1
-        deq.append(tmp1)
-    if 1 <= tmp2 and not visited[tmp2]:
-        visited[tmp2] = True
-        gogo[tmp2] = count+1
-        deq.append(tmp2)
-
-print(gogo[G] if gogo[G] != 0 else "use the stairs")
+f,s,g,u,d=map(int,input().split())
+r=0
+while r<f and 0<s and s<= f:
+    if s==g:
+        print(r)
+        exit()
+    s-=d if s>g and s>d or s<g and s+u>f else-u
+    r+=1
+print("use the stairs")
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,29 +1,62 @@
-import sys
-import heapq
-input =sys.stdin.readline
-INF = 1e7
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-def solve():
-    V, E = map(int, sys.stdin.readline().split())
-    K = int(sys.stdin.readline())
-    adj = [dict() for _ in range(V + 1)]
-    for _ in range(E):
-        u, v, w = map(int, input().split())
-        adj[u][v] = min(adj[u].get(v,INF),w)
-    dist = [INF for _ in range(V + 1)]
-    dist[K] = 0
-    q = [(0, K)]
-    
-    while q:
-        c, p = heapq.heappop(q)
-        if c > dist[p]:
-            continue
-        for v, w in adj[p].items():
-            if dist[v] <= c+w:
-                continue
-            dist[v] = c+w
-            heapq.heappush(q, (c+w, v))
-    print('\n'.join(map(str,[r if r < INF else "INF" for r in dist[1:]])))
+def change(d, c):
+    if(c == "L"):
+        d = (d-1)%4
+    else:
+        d = (d+1)%4
+    return d
         
-if __name__=='__main__':
-    solve()
+def start():
+    cnt = 0
+    # 뱀 위치 표시
+    x = 0
+    y = 0
+    arr[x][y] = 3
+    d = 0
+    idx = 0
+    # 뱀의 꼬리 ~ 머리까지의 정보를 q에 담기
+    q = []
+    q.append((x,y))
+    while(True):
+        # 몇초에 방향 전환 하는지
+        if(game[idx][0] == cnt):
+            d = change(d, game[idx][1])
+            idx += 1
+        print(q)
+        nx = x + dx[d]
+        ny = y + dy[d]
+        # 범위안에 있고, 자기자신이 아니라면
+        if(0<=nx<n and 0<=ny<n and arr[nx][ny] != 3):
+            # 사과를 먹으면 꼬리는 그대로
+            if(arr[nx][ny] == 1):
+                arr[nx][ny] = 3
+                q.append((nx,ny))
+            # 사과를 못먹으면 꼬리 하나 떼기
+            elif(arr[nx][ny] == 0):
+                arr[nx][ny] = 3
+                q.append((nx,ny))
+                tx, ty = q.pop(0)
+                arr[tx][ty] = 0
+            x = nx
+            y = ny
+            cnt += 1
+        else:
+            cnt += 1
+            break
+    return cnt
+
+n = int(input())
+k = int(input())
+arr = [[0]*n for _ in range(n)]
+for _ in range(k):
+    a, b = map(int, input().split())
+    arr[a-1][b-1] = 1
+l = int(input())
+game = [[0,0]]*10000
+for i in range(l):
+    x, c = input().split()
+    game[i] = [int(x), c]
+res = start()
+print(res)
