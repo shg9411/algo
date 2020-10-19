40ca0dd9c392a134694349872993183835816bc0
shg9411 <shg9411@naver.com>
2020-10-19 21:09:01 +0900

    2020-10-19_algo

--- algo_py/boj/bj6593.py
+++ algo_py/boj/bj6593.py
@@ -11,13 +11,12 @@ def solve():
         bd = [[] for _ in range(L)]
         v = [[[False for _ in range(C)]
               for _ in range(R)] for _ in range(L)]
-        S = [-1, -1, -1]
         for i in range(L):
             for j in range(R):
                 bd[i].append(list(input().rstrip()))
                 for k in range(C):
                     if bd[i][j][k] == 'S':
-                        S = [i, j, k]
+                        S = (i, j, k)
                         v[i][j][k] = True
             input()
         q = deque([S])
@@ -35,7 +34,7 @@ def solve():
                         break
                     if bd[k][X][j] == '.' and not v[k][X][j]:
                         v[k][X][j] = True
-                        q.append([k, X, j])
+                        q.append((k, X, j))
                 X += 2
                 if X < R:
                     if bd[k][X][j] == 'E':
@@ -43,7 +42,7 @@ def solve():
                         break
                     if bd[k][X][j] == '.' and not v[k][X][j]:
                         v[k][X][j] = True
-                        q.append([k, X, j])
+                        q.append((k, X, j))
                 Y = j-1
                 if 0 <= Y:
                     if bd[k][i][Y] == 'E':
@@ -51,7 +50,7 @@ def solve():
                         break
                     if bd[k][i][Y] == '.' and not v[k][i][Y]:
                         v[k][i][Y] = True
-                        q.append([k, i, Y])
+                        q.append((k, i, Y))
                 Y += 2
                 if Y < C:
                     if bd[k][i][Y] == 'E':
@@ -59,7 +58,7 @@ def solve():
                         break
                     if bd[k][i][Y] == '.' and not v[k][i][Y]:
                         v[k][i][Y] = True
-                        q.append([k, i, Y])
+                        q.append((k, i, Y))
                 Z = k-1
                 if 0 <= Z:
                     if bd[Z][i][j] == 'E':
@@ -67,7 +66,7 @@ def solve():
                         break
                     if bd[Z][i][j] == '.' and not v[Z][i][j]:
                         v[Z][i][j] = True
-                        q.append([Z, i, j])
+                        q.append((Z, i, j))
                 Z += 2
                 if Z < L:
                     if bd[Z][i][j] == 'E':
@@ -75,7 +74,7 @@ def solve():
                         break
                     if bd[Z][i][j] == '.' and not v[Z][i][j]:
                         v[Z][i][j] = True
-                        q.append([Z, i, j])
+                        q.append((Z, i, j))
             if can:
                 break
         print("Escaped in {} minute(s).".format(cnt) if can else "Trapped!")
