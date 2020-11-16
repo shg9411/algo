ec588e7e58e861d3e29870dbfb5d112209a8ef16
shg9411 <shg9411@naver.com>
2020-11-16 12:26:00 +0900

    2020-11-16_algo

--- algo_py/boj/bj15988.py
+++ algo_py/boj/bj15988.py
@@ -1,10 +1,17 @@
 import sys
-input = sys.stdin.readline
-cnt = [0, 1, 2, 4]
-MOD = 1000000009
-
-for i in range(4, 1000001):
-    cnt.append(sum(cnt[-3:]) % MOD)
-
-for _ in range(int(input())):
-    print(cnt[int(input())])
+def solve():
+    c=[0,1,2,4]
+    MOD=10**9+9
+    tmp=3
+    t=int(input())
+    q=list(map(int,sys.stdin.read().split()))
+    m=max(q)
+    for i in range(4,m+1):
+        tmp=(tmp-c[-4]+c[-1])%MOD
+        c.append(tmp)
+    r=[]
+    for num in q:
+        r.append(c[num])
+    print('\n'.join(map(str,r)))
+if __name__=='__main__':
+    solve()
--- algo_py/boj/bj4195.py
+++ algo_py/boj/bj4195.py
@@ -1,37 +1,43 @@
 import sys
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 t = int(input())
 
+def solve():
+    def find(x):
+        if x == parent[x]:
+            return x
+        parent[x] = find(parent[x])
+        return parent[x]
+    
+    
+    def union(x, y):
+        x = find(x)
+        y = find(y)
+        if x != y:
+            parent[x] = y
+            num[y] += num[x]
+            num[x] = 1
+        return num[y]
+    
+    
+    res = []
+    for _ in range(t):
+        f = int(input())
+        cnt = 1
+        parent = [i for i in range(f*2+1)]
+        num = [1]*(f*2+1)
+        freind = dict()
+        for _ in range(f):
+            a, b = input().split()
+            if a not in freind:
+                freind[a] = cnt
+                cnt += 1
+            if b not in freind:
+                freind[b] = cnt
+                cnt += 1
+            res.append(union(freind[a], freind[b]))
+    print('\n'.join(map(str,res)))
 
-def find(x):
-    if x == parent[x]:
-        return x
-    parent[x] = find(parent[x])
-    return parent[x]
-
-
-def union(x, y):
-    x = find(x)
-    y = find(y)
-    if x != y:
-        parent[x] = y
-        num[y] += num[x]
-        num[x] = 1
-    return num[y]
-
-
-for _ in range(t):
-    f = int(input())
-    cnt = 1
-    parent = [i for i in range(f*2)]
-    num = [1 for _ in range(f*2)]
-    freind = dict()
-    for _ in range(f):
-        a, b = input().split()
-        if a not in freind:
-            freind[a] = cnt
-            cnt += 1
-        if b not in freind:
-            freind[b] = cnt
-            cnt += 1
-        print(union(freind[a], freind[b]))
+if __name__=='__main__':
+    solve()
