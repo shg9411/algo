b267a447b8d8ea3f1a62a03717b11214d48a257d
shg9411 <shg9411@naver.com>
2020-12-21 22:13:55 +0900

    2020-12-21_algo

---
+++ algo_py/boj/bj15486.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+job = []
+dp = [0 for _ in range(1500001)]
+for _ in range(n):
+    job.append(list(map(int, input().split())))
+for i in range(n-1, -1, -1):
+    if job[i][0] + i > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i+job[i][0]]+job[i][1])
+
+print(dp[0])
