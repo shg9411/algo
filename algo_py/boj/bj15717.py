dce5a29aa5cf33d778e9807d40051425cbbc37ab
shg9411 <shg9411@naver.com>
2021-01-09 13:15:19 +0900

    2021-01-09_algo

---
+++ algo_py/boj/bj15717.py
@@ -0,0 +1,1 @@
+print(pow(2, n-1, 10**9+7)if(n := int(input())) > 1 else 1)
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,23 +1,3 @@
-import sys
-
-def perfect(c):
-    t = ['[', ']', '(', ')']
-    if c in t:
-        return True
-    else:
-        return False
-
-while True:
-    readt=sys.stdin.readline().rstrip()
-    if readt==".":
-        break
-    toparse=(''.join(filter(perfect, readt))).replace('()', '(1,)').replace(')(', '),(').replace('][', '],[').replace(')[', '),[').replace('](', '],(')
-    print(toparse)
-    try:
-        if toparse.startswith("]"):
-            print("no")
-        else:
-            eval("["+toparse+"]")
-            print("yes")
-    except:
-        print("no")
+print('*'*100)
+print("A","$"*97,"*","a",sep='')
+print('*'*100)
