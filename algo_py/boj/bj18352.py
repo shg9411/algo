24de99c7c9d930203e6df2bf49539ca0af05afa8
shg9411 <shg9411@naver.com>
2021-01-13 13:28:35 +0900

    2021-01-13_algo

--- algo_py/boj/bj18352.py
+++ algo_py/boj/bj18352.py
@@ -1,32 +1,35 @@
 import sys
 import heapq
-
 input = sys.stdin.readline
-INF = sys.maxsize
+INF = 300001
 
-n, m, k, x = map(int, input().split())
-edge = [dict() for _ in range(n+1)]
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    edge[a][b] = 1
+def solve():
+    n, m, k, x = map(int, input().split())
+    edge = [dict() for _ in range(n+1)]
 
-dist = [INF for _ in range(n+1)]
-dist[x] = 0
+    for _ in range(m):
+        a, b = map(int, input().split())
+        edge[a][b] = 1
 
-pq = [(0, x)]
+    dist = [INF for _ in range(n+1)]
+    dist[x] = 0
 
-while pq:
-    d1, v1 = heapq.heappop(pq)
-    if d1 == k:
-        break
-    for v2, d2 in edge[v1].items():
-        if dist[v1]+d2 < dist[v2]:
-            dist[v2] = dist[v1]+d2
-            heapq.heappush(pq, (dist[v2], v2))
+    pq = [(0, x)]
 
-res = []
-for i, v in enumerate(dist):
-    if v == k:
-        res.append(i)
-print('\n'.join(map(str, res)) if res else -1)
+    while pq:
+        d1, v1 = heapq.heappop(pq)
+        if dist[v1] < d1:
+            continue
+        if d1 == k:
+            break
+        for v2, d2 in edge[v1].items():
+            if dist[v1]+d2 < dist[v2]:
+                dist[v2] = dist[v1]+d2
+                heapq.heappush(pq, (dist[v2], v2))
+
+    print('\n'.join(map(str, (i for i, v in enumerate(dist) if v == k))) or -1)
+
+
+if __name__ == '__main__':
+    solve()
--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,20 +1,28 @@
-n = int(input())
-arr = ["" for _ in range(8)] #수의 길이가 최대 8이므로 길이 8짜리 리스트를 만듬(리스트의 인덱스에 대한 설명 : index 7->1의 자리, 6->10의 자리 .... 0->1000만의자리)
-result = {} #알파벳과 숫자를 키와 값으로 대응시킬 딕셔너리
-sum = 0
-for _ in range(n):
-  word = input()
-  #리스트arr의 마지막 방과 현재 입력받은 단어의 마지막 알파벳을 일치시키며 단어의 뒤에서부터 앞쪽으로 리스트arr에 넣어준다. 즉, 알파벳들이 그 자리수에 맞게 arr에 차곡차곡 쌓이게 된다.
-  for i in range(len(word)): 
-    arr[7-i]+=word[len(word)-1-i]
-print(arr)
-i = 9 
-for k in range(len(arr)):
-  if arr[k]: #arr[k]가 비어있지 않다면
-    for alpha in arr[k]: 
+import sys
+import heapq
+input = sys.stdin.readline
+INF=300001
+def solve():
+    n, m, k, x = map(int, input().split())
+    edge = [dict() for _ in range(n+1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        edge[a][b] = 1
+    dist = [INF for _ in range(n+1)]
+    dist[x] = 0
+    pq = [(0, x)]
+    while pq:
+        d1, v1 = heapq.heappop(pq)
+        if dist[v1] < d1:
+            continue
+        if d1 == k:
+            break
+        for v2, d2 in edge[v1].items():
+            if dist[v1]+d2 < dist[v2]:
+                dist[v2] = dist[v1]+d2
+                heapq.heappush(pq, (dist[v2], v2))
+    print('\n'.join(map(str, (i for i, v in enumerate(dist) if v == k))) or -1)
 
-      if alpha not in result: #딕셔너리에 현재 계산하려고하는 알파벳이 없는 상태라면
-        result[alpha] = i
-        i-=1
-      sum+=(10**(7-k))*result[alpha] #딕셔너리에서 해당 알파벳에 대응하는 숫자를 찾아내어 자릿수와 곱하여 sum에 누적.
-print(sum)
+    
+if __name__ == '__main__':
+    solve()
