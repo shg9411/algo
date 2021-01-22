473f5b723c17f71c4875b056613a57b892ff1467
shg9411 <shg9411@naver.com>
2021-01-22 22:22:33 +0900

    2021-01-22_algo

--- algo_py/boj/bj1976.py
+++ algo_py/boj/bj1976.py
@@ -2,36 +2,39 @@ import sys
 input = sys.stdin.readline
 
 
-def find(x):
-    if x == parent[x]:
-        return x
-    parent[x] = find(parent[x])
-    return parent[x]
+def main():
+
+    def find(x):
+        if x == p[x]:
+            return x
+        p[x] = find(p[x])
+        return p[x]
+
+    def union(x, y):
+        x = find(x)
+        y = find(y)
+        if x < y:
+            p[y] = x
+        else:
+            p[x] = y
+
+    n = int(input())
+    p = [i for i in range(n)]
+    m = int(input())
+    for i in range(n):
+        tmp = list(map(int, input().split()))
+        for j in range(n):
+            if i != j and tmp[j] == 1:
+                union(i, j)
+
+    ans = set()
+    for n in map(lambda x: int(x)-1, input().split()):
+        ans.add(find(n))
+        if len(ans) > 1:
+            print("NO")
+            exit()
+    print("YES")
 
 
-def union(x, y):
-    x = find(x)
-    y = find(y)
-    if x < y:
-        parent[y] = x
-    else:
-        parent[x] = y
-
-
-n = int(input())
-parent = [i for i in range(n)]
-m = int(input())
-for i in range(n):
-    tmp = list(map(int, input().split()))
-    for j in range(n):
-        if i != j and tmp[j] == 1:
-            union(i, j)
-
-trap = list(map(int, input().split()))
-ans = set()
-for num in trap:
-    ans.add(find(num-1))
-    if len(ans)>1:
-        print("NO")
-        exit()
-print("YES")
+if __name__ == '__main__':
+    main()
