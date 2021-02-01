e605415fb15df8966e4619f467fa58c9f09f2d92
shg9411 <shg9411@naver.com>
2021-02-01 12:27:24 +0900

    2021-02-01_algo

--- algo_py/boj/bj2798.py
+++ algo_py/boj/bj2798.py
@@ -1,22 +1,7 @@
-N, M = map(int, input().split())
-cards = list(reversed(sorted(map(int, input().split()))))
-sumSet = set()
-blackjack = False
-for i in range(N-2):
-    for j in range(i+1, N-1):
-        for k in range(j+1, N):
-            sum = cards[i] + cards[j] + cards[k]
-            if sum == M:
-                blackjack = True
-                break
-            elif sum < M:
-                sumSet.add(sum)
-                break
-        if blackjack:
-            break
-    if blackjack:
-        break
-if blackjack:
-    print(M)
-else:
-    print(max(sumSet))
+from itertools import combinations
+n, m = map(int, input().split())
+res = -1
+for c in combinations(map(int, input().split()), 3):
+    if (r := sum(c)) <= m:
+        res = max(r, res)
+print(res)
