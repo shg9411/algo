21dcbdcb9a5895edee9b0fce65e30918ebc74497
shg9411 <shg9411@naver.com>
2021-02-03 21:53:34 +0900

    2021-02-03_algo

--- algo_py/boj/tmp.py
+++ algo_py/boj/tmp.py
@@ -1,9 +1,5 @@
-import io
-import os
 import sys
-input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 
-if __name__ == "__main__":
-    n = int(input())
-    sys.stdout.write(
-        "\n".join(map(str, sorted([int(input()) for _ in range(n)])[::-1])))
+one = sys.stdin.read(1)
+two = sys.stdin.read(1)
+print(one,two)
