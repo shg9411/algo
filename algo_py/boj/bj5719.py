a7b409455f0c9b1a9cc123debad9bdcbe779798f
shg9411 <shg9411@naver.com>
2021-02-13 22:07:33 +0900

    2021-02-13_algo

--- algo_py/boj/bj5719.py
+++ algo_py/boj/bj5719.py
@@ -7,10 +7,7 @@ input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 
 
 def solve():
-    def dijkstra(s, e):
-        dist = [sys.maxsize for _ in range(n)]
-        dist[s] = 0
-        q = [(0, s)]
+    def dijkstra(dist, q, e):
         while q:
             cost, des = heapq.heappop(q)
             if dist[des] != cost:
@@ -22,28 +19,7 @@ def solve():
                 if dist[to] > val:
                     dist[to] = val
                     heapq.heappush(q, (val, to))
-        q = deque([e])
-        while q:
-            des = q.popleft()
-            for i in range(n):
-                if des in adj[i] and dist[i]+adj[i][des] == dist[des]:
-                    adj[i].pop(des)
-                    q.append(i)
-        dist = [sys.maxsize for _ in range(n)]
-        dist[s] = 0
-        q = [(0, s)]
-        while q:
-            cost, des = heapq.heappop(q)
-            if dist[des] != cost:
-                continue
-            if des == e:
-                break
-            for to, val in adj[des].items():
-                val += cost
-                if dist[to] > val:
-                    dist[to] = val
-                    heapq.heappush(q, (val, to))
-        return dist[e]
+        return dist
 
     while 1:
         n, m = map(int, input().split())
@@ -54,8 +30,22 @@ def solve():
         for _ in range(m):
             u, v, p = map(int, input().split())
             adj[u][v] = p
-        val = dijkstra(s, e)
-        print(val if val != sys.maxsize else -1)
+        dist = [sys.maxsize for _ in range(n)]
+        dist[s] = 0
+        q = [(0, s)]
+        dist = dijkstra(dist, q, e)
+        q = deque([e])
+        while q:
+            des = q.popleft()
+            for i in range(n):
+                if des in adj[i] and dist[i]+adj[i][des] == dist[des]:
+                    adj[i].pop(des)
+                    q.append(i)
+        dist = [sys.maxsize for _ in range(n)]
+        dist[s] = 0
+        q = [(0, s)]
+        dist = dijkstra(dist, q, e)
+        print(dist[e] if dist[e] != sys.maxsize else -1)
 
 
 if __name__ == '__main__':
