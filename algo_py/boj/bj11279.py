b886e410e9d4515f9d57d8f86d05f9bf897d8c1b
shg9411 <shg9411@naver.com>
2021-02-22 22:44:20 +0900

    2021-02-22_algo

--- algo_py/boj/bj11279.py
+++ algo_py/boj/bj11279.py
@@ -1,10 +1,20 @@
+import io
+import os
 import sys
 import heapq
-q = []
-res = []
-for i in map(int, sys.stdin.read().splitlines()[1:]):
-    if i:
-        heapq.heappush(q, -i)
-    else:
-        res.append(-heapq.heappop(q) if q else 0)
-print('\n'.join(map(str, res)))
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+
+def solve():
+    q = []
+    res = []
+    for _ in range(int(input())):
+        if (a := int(input())):
+            heapq.heappush(q, -a)
+        else:
+            res.append(-heapq.heappop(q) if q else 0)
+    sys.stdout.write('\n'.join(map(str, res)))
+
+
+if __name__ == '__main__':
+    solve()
