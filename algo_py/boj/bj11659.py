51ad1e4c1f9d0fb6fdaf4ddfa3dcccc9a3e96e47
Song_H_G <34259849+shg9411@users.noreply.github.com>
2021-03-12 09:18:55 +0900

    Update bj11659.py

--- algo_py/boj/bj11659.py
+++ algo_py/boj/bj11659.py
@@ -1,10 +1,20 @@
+import io
+import os
 import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-num = list(map(int, input().split()))
-total = [0]
-for n in num:
-    total.append(total[-1] + n)
-for _ in range(m):
-    i, j = map(int, input().split())
-    print(total[j]-total[i-1])
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+
+def solve():
+    n, m = map(int, input().split())
+    total = [0]
+    for n in map(int, input().split()):
+        total.append(total[-1] + n)
+    res = []
+    for _ in range(m):
+        i, j = map(int, input().split())
+        res.append(total[j]-total[i-1])
+    sys.stdout.write('\n'.join(map(str, res)))
+
+
+if __name__ == '__main__':
+    solve()
