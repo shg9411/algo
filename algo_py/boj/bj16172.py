3b029bb6bdbb6bab37d1470fcb4883ca5c03c86a
Song_H_G <34259849+shg9411@users.noreply.github.com>
2021-05-26 20:26:25 +0900

    Update bj16172.py

--- algo_py/boj/bj16172.py
+++ algo_py/boj/bj16172.py
@@ -1,2 +1,34 @@
-s = ''.join(c*(c > '9') for c in input())
-print(+(input() in s))
+T = ''.join((c for c in input().rstrip() if c.isalpha()))
+P = input().rstrip()
+
+
+def getPi(pattern):
+    strlen = len(pattern)
+    j = 0
+    pi = [0 for _ in range(strlen)]
+    for i in range(1, strlen):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = pi[j-1]
+        if pattern[i] == pattern[j]:
+            j += 1
+            pi[i] = j
+
+    return pi
+
+
+def kmp(text, pattern):
+    pi = getPi(pattern)
+    tLen = len(text)
+    sLen = len(pattern)
+    j = 0
+    for i in range(tLen):
+        while j > 0 and text[i] != pattern[j]:
+            j = pi[j-1]
+        if text[i] == pattern[j]:
+            if j == sLen-1:
+                return 1
+            j += 1
+    return 0
+
+
+print(kmp(T, P))
