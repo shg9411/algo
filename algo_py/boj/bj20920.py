0a0ff845f36547f12df8a411980b4b755e6b766a
shg9411 <shg9411@naver.com>
2021-06-24 20:05:45 +0900

    edit algo

--- algo_py/boj/bj20920.py
+++ algo_py/boj/bj20920.py
@@ -1,15 +1,6 @@
 import sys
-import heapq
-input = sys.stdin.readline
+from collections import Counter
 
-n, m = map(int, input().split())
-dictObj = {}
-for _ in range(n):
-    word = input().rstrip()
-    length = len(word)
-    if length < m:
-        continue
-    dictObj[word] = dictObj.get(word, 0)+1
-
-for k, _ in sorted(dictObj.items(), key=lambda x: (-x[1], -len(x[0]), x[0])):
+_, m = map(int, input().split())
+for k, _ in sorted(Counter(w for w in sys.stdin.read().split() if len(w) >= m).items(), key=lambda x: (-x[1], -len(x[0]), x[0])):
     print(k)
